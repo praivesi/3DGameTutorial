a5b5e641348deca7b27803e23b9420980b9ab7a3
praivesi <praivesi@gmail.com>
2021-09-24 22:47:57 +0900

    feat: add collision detection and audio sources

--- Assets/Scripts/PlayerBall.cs
+++ Assets/Scripts/PlayerBall.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class PlayerBall : MonoBehaviour
 {
     public float jumpPower;
+    public int itemCount;
+
+    bool isJumping;
     Rigidbody rigid;
+    AudioSource audio;
 
     void Awake()
     {
+        isJumping = false;
         rigid = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !isJumping)
         {
+            isJumping = true;
             rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
         }
     }
@@ -27,5 +34,24 @@ public class PlayerBall : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
 
         rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Floor")
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Item")
+        {
+            itemCount++;
+            audio.Play();
+
+            other.gameObject.SetActive(false);
+        }
     }
 }
